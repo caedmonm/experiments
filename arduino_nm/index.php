bb6641d138e1c98aef4f4741c8ead541aa752b2b
Caedmon Mullin <caedmon.m@gmail.com>
2018-08-09 08:40:04 +0100

    .

--- arduino_nm/index.php
+++ arduino_nm/index.php
@@ -1,9 +1,11 @@
 <?
-if(isset($_POST['val'])){
+if(isset($_POST['pos'])){
     $file = 'pos.txt';
     $handle = fopen($file, 'w') or die('Cannot open file:  '.$file);
-    $data = intval($_POST['val']);
+    $data = intval($_POST['pos']);
     fwrite($handle, $data);
+    echo $data;
+    exit;
 }
 ?>
 <html>
@@ -11,9 +13,13 @@ if(isset($_POST['val'])){
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     </head>
     <body>
-        <input type="number" value="90" style="border-radius:4px; border:none; background-color: #eee; padding: 10px 20px" />
+        <input id="pos" type="number" value="90" style="border-radius:4px; border:none; background-color: #eee; padding: 10px 20px" />
         <script>
-
+            $(document).on("change","#pos",function(){
+                $.post("./index.php",{pos:$(this).val()},function(d){
+                    console.log('set to:',d);
+                });
+            });
         </script>
     </body>
 </html>
