0c4b38e3a46fe41e42e95c8935e04c54a478abaa
Caedmon Mullin <caedmon.m@gmail.com>
2018-08-09 12:38:28 +0100

    .

--- arduino_nm/arduino_nm.ino
+++ arduino_nm/arduino_nm.ino
@@ -54,35 +54,35 @@ String getJsonHTTP(String host, String url){
     HTTPClient http;
     String payload;
 
-    Serial.print("[HTTP] begin...\n");
+    //Serial.print("[HTTP] begin...\n");
 
     http.begin("http://"+host+url); //HTTP
 
-    Serial.print("[HTTP] GET...\n");
+    //Serial.print("[HTTP] GET...\n");
     // start connection and send HTTP header
     int httpCode = http.GET();
 
     // httpCode will be negative on error
     if(httpCode > 0) {
         // HTTP header has been send and Server response header has been handled
-        Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+        //Serial.printf("[HTTP] GET... code: %d\n", httpCode);
 
         // file found at server
         if(httpCode == HTTP_CODE_OK) {
             payload = http.getString();
         }
     } else {
-        Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+        //Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
     }
 
     http.end();
 
 
     #ifdef PRINT_PAYLOAD
-        Serial.println("reply was:");
-        Serial.println("==========");
-        Serial.println(payload);
-        Serial.println("==========");
+        //Serial.println("reply was:");
+        //Serial.println("==========");
+        //Serial.println(payload);
+        //Serial.println("==========");
     #endif
     return payload;
 }
@@ -122,10 +122,10 @@ void loop() {
    String json = getJsonHTTP("bigpebbles.co.uk","/experiments/arduino_nm/pos.txt");
    JsonObject& root = jsonBuffer.parseObject(json);
    if (!root.success()) {
-    Serial.println("parseObject() failed");
+    //Serial.println("parseObject() failed");
     return;
   }
-    Serial.println("json parsed");
+    //Serial.println("json parsed");
     int pos = root["position"];
   
    MoveServoToPosition(pos,0);
