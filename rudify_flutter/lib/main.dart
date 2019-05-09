fc1658a7ecf4a8267f0e3debbc32ffd1fc09c7bf
Caedmon Mullin <caedmon.m@gmail.com>
2019-05-09 13:48:19 +0100

    voice options

--- rudify_flutter/lib/main.dart
+++ rudify_flutter/lib/main.dart
@@ -12,15 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+        fontFamily: 'Nanum',
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'RUDIFY!'),
@@ -45,25 +37,108 @@ class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
-AudioPlayer advancedPlayer = new AudioPlayer();
+
+var players = [
+  new AudioPlayer(),
+  new AudioPlayer(),
+  new AudioPlayer(),
+  new AudioPlayer()
+];
 
 class _MyHomePageState extends State<MyHomePage> {
   String _text = "";
-  
-  var wordLists = [["spiny","rotten","mouldy","squelchy","stinky","smelly","slimy","muddy","squishy","wandering","scaly","hairy","furry","itching","scratching"],["toilet","dungeon","garbage","arm pit","pant","fridge","suburban","urban","tree","river","space","field","lake","ocean","sea","wood","forest","desert","toe","butt","boob"],["hobbit","cucumber","fudge","slime","stink","turd","smell","tussock","clod","worm","slug","banana","bunion","dribble","plop","toenail"]];
+  int _voice = 0;
+
+  var wordLists = [
+    [
+      "spiny",
+      "rotten",
+      "mouldy",
+      "squelchy",
+      "stinky",
+      "smelly",
+      "slimy",
+      "muddy",
+      "squishy",
+      "wandering",
+      "scaly",
+      "hairy",
+      "furry",
+      "itching",
+      "scratching"
+    ],
+    [
+      "toilet",
+      "dungeon",
+      "garbage",
+      "arm pit",
+      "pant",
+      "fridge",
+      "suburban",
+      "urban",
+      "tree",
+      "river",
+      "space",
+      "field",
+      "lake",
+      "ocean",
+      "sea",
+      "wood",
+      "forest",
+      "desert",
+      "toe",
+      "butt",
+      "boob"
+    ],
+    [
+      "hobbit",
+      "cucumber",
+      "fudge",
+      "slime",
+      "stink",
+      "turd",
+      "smell",
+      "tussock",
+      "clod",
+      "worm",
+      "slug",
+      "banana",
+      "bunion",
+      "dribble",
+      "plop",
+      "toenail"
+    ]
+  ];
   FlutterTts flutterTts = new FlutterTts();
-  
-  AudioCache audioCache = new AudioCache(fixedPlayer: advancedPlayer);
-  
-  Future _playWord(say,i) async {
-    print(i);
-    audioCache.play('voice1/'+say[i]+'.mp3');
-    i++;
-    if(i<4){
-      advancedPlayer.completionHandler = () => (_playWord(say,i));
-    } else {
-      advancedPlayer.completionHandler = () => (null);
-    }
+
+  var audioCaches = [
+    new AudioCache(fixedPlayer: players[0]),
+    new AudioCache(fixedPlayer: players[1]),
+    new AudioCache(fixedPlayer: players[2]),
+    new AudioCache(fixedPlayer: players[3]),
+  ];
+
+  Future _playWord(say, i) async {
+    Duration _duration = new Duration();
+    Duration _position = new Duration();
+    List overlap = [.4, .8, .4, 1];
+
+    audioCaches[i].play("voice1/" + say[i] + ".mp3");
+
+    players[i].durationHandler = (d) => setState(() {
+          _duration = d;
+        });
+
+    players[i].positionHandler = (p) => setState(() {
+          _position = p;
+          if (_duration * overlap[i] < _position) {
+            if (i < 5) {
+              _playWord(say, (i + 1));
+            } else {
+              return null;
+            }
+          }
+        });
   }
 
   Future _getWords() async {
@@ -73,63 +148,76 @@ class _MyHomePageState extends State<MyHomePage> {
       say.add((wordLists[0]..shuffle()).first);
       say.add((wordLists[1]..shuffle()).first);
       say.add((wordLists[2]..shuffle()).first);
-      
+
       String write = "You're a ";
       write += say[1];
-      write += " "+say[2];
-      write += " "+say[3];
-      
+      write += " " + say[2];
+      write += " " + say[3];
+
       _text = write;
     });
-    // await flutterTts.speak(_text);
-    // await flutterTts.setVolume(1.0);
-    audioCache.loadAll(['voice1/'+say[0]+'.mp3','voice1/'+say[1]+'.mp3','voice1/'+say[2]+'.mp3','voice1/'+say[3]+'.mp3']);
-    _playWord(say,0);
-    
-    // advancedPlayer.completionHandler = () => (){
-    //   audioCache.play('voice1/'+say[1]+'.mp3');
 
-    // };
+    if (_voice == 0) {
+      await flutterTts.speak(_text);
+      await flutterTts.setVolume(1.0);
+    } else {
+      audioCaches[0].load("voice${_voice}/" + say[0] + '.mp3');
+      audioCaches[1].load("voice${_voice}/" + say[1] + '.mp3');
+      audioCaches[2].load("voice${_voice}/" + say[2] + '.mp3');
+      audioCaches[3].load("voice${_voice}/" + say[3] + '.mp3');
+      _playWord(say, 0);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              '$_text',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _getWords,
-        tooltip: 'Increment',
-        icon: Icon(Icons.chat_bubble_outline),
-        backgroundColor: Colors.pink,
-        label: Text("RUDIFY!"),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('$_text',
+                    style: TextStyle(fontFamily: 'Nanum', fontSize: 40)),
+              ],
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: _getWords,
+          tooltip: 'Increment',
+          icon: Icon(Icons.chat_bubble_outline),
+          backgroundColor: Colors.pink,
+          label: Text("RUDIFY!"),
+        ),
+        bottomNavigationBar: BottomAppBar(
+            child: Row(
+          children: <Widget>[
+            FlatButton(
+                onPressed: () => {_voice = 0},
+                color: Colors.orange,
+                child: Row(
+                  children: <Widget>[
+                    Text(
+                      "Robo",
+                    ),
+                  ],
+                )),
+            FlatButton(
+                onPressed: () => {_voice = 1},
+                color: Colors.orange,
+                child: Row(
+                  children: <Widget>[
+                    Text(
+                      "Ivo",
+                    ),
+                  ],
+                )),
+          ],
+        )));
   }
 }
